2ae1d3aad152a360081cf55aa5c5d097479b429e
codemaestro🚀 <mkyelugift@gmail.com>
2026-01-07 07:09:16 +0200

    Update APP_LOGO_URL to new logo image

--- client/src/utils/constants.ts
+++ client/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const APP_LOGO_URL = 'https://www.vwaza.com/vw-logo.png';
+export const APP_LOGO_URL = 'https://w7.pngwing.com/pngs/208/848/png-transparent-musical-note-logo-clef-music-logo-design-other-text-musical-composition-thumbnail.png';
 
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:6155';
 
